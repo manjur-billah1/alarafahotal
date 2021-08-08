a6b8fb628be29b1c6184d48912728344ffdd835a
manjur-billah1 <87940371+manjur-billah1@users.noreply.github.com>
2021-08-08 22:47:27 +0600

    Add files via upload

---
+++ scriptmode.js
@@ -0,0 +1,45 @@
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByTagName('body')[0].classList.toggle("nothing");
+});
+
+
+
+document.getElementById("toggle").addEventListener("click", function(){
+    document.getElementsByClassName('banner')[0].classList.toggle("banner-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('logo')[0].classList.toggle("logo-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('Mix-div')[0].classList.toggle("Mix-div-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('survicess')[0].classList.toggle("survicess-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('title-text')[0].classList.toggle("title-text-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('survices-img')[0].classList.toggle("survices-img-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('icon')[0].classList.toggle("icon-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('survices')[0].classList.toggle("survices-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('dishes')[0].classList.toggle("dishes-dark");
+});
+
+document.getElementById("toggle").addEventListener("click", function() {
+  document.getElementsByClassName('single-dishes')[0].classList.toggle("single-dishes-dark");
+});
